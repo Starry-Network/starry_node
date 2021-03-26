0e155886e1e4d54c57885004928b7db4321a3b72
poria <mon.samuel@qq.com>
2021-03-26 17:03:37 +0000

    add link function fot graph pallet

--- pallets/pallet-graph/src/lib.rs
+++ pallets/pallet-graph/src/lib.rs
@@ -1,7 +1,9 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![recursion_limit = "128"]
 use frame_support::{
-    decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult, ensure,
+    decl_error, decl_event, decl_module, decl_storage,
+    dispatch::{DispatchError, DispatchResult},
+    ensure,
     traits::Get,
 };
 use frame_system::ensure_signed;
@@ -24,7 +26,9 @@ pub trait Config: frame_system::Config + pallet_collection::Config + pallet_nft:
 
 decl_storage! {
     trait Store for Module<T: Config> as GraphModule {
-        GraphCreator get(fn graph_creator): map hasher(blake2_128_concat)  T::Hash => T::AccountId;
+        // GraphCreator get(fn graph_creator): map hasher(blake2_128_concat)  T::Hash => T::AccountId;
+        // Child(collection_id, token_id) => Parent(collection_id, token_id)
+        ChildToParent get(fn child_to_parent): map hasher(blake2_128_concat) (T::Hash, u128) => (T::Hash, u128)
     }
 }
 
@@ -41,9 +45,11 @@ decl_event!(
 
 decl_error! {
     pub enum Error for Module<T: Config> {
-        NoneValue,
-        StorageOverflow,
-        GraphNotFound
+        GraphNotFound,
+        PermissionDenied,
+        ParentCollectionNotFound,
+        RootTokenNotFound,
+        CanNotLinkAncestorToDescendant,
     }
 }
 
@@ -53,12 +59,108 @@ decl_module! {
 
         fn deposit_event() = default;
 
-		// link to other token
+        // link to other token
+        #[weight = 10_000]
+        pub fn link(origin, child_collection_id: T::Hash, child_token_id: u128, parent_collection_id: T::Hash, parent_token_id: u128) -> DispatchResult {
+            ensure!(
+                <pallet_collection::Collections<T>>::contains_key(parent_collection_id),
+                Error::<T>::ParentCollectionNotFound
+            );
+            ensure!(
+                <pallet_nft::Tokens<T>>::contains_key(parent_collection_id, parent_token_id),
+                <pallet_nft::Error<T>>::TokenNotFound
+            );
+
+
+            let who = ensure_signed(origin.clone())?;
+
+            if ChildToParent::<T>::contains_key((child_collection_id, child_token_id)) {
+                // if token in ChildToParent, it's owner is graph pallet.
+                let root_token_owner = Self::find_root_owner(child_collection_id, child_token_id)?;
+                ensure!(&root_token_owner == &who, Error::<T>::PermissionDenied);
+            } else {
+                // token's owner should be user
+                <pallet_nft::Module<T>>::transfer_non_fungible(origin, Self::account_id(), child_collection_id, child_token_id, 1)?;
+            }
+
+            // if parent token's owner is user, it can be a root token, so don't check
+            let parent_token = <pallet_nft::Module<T>>::tokens(parent_collection_id, parent_token_id);
+
+            if parent_token.owner == Self::account_id() {
+                let child_is_parent_ancestor = Self::is_ancestor((child_collection_id, child_token_id), (parent_collection_id, parent_token_id))?;
+                // ensure!(, Error::<T>::CanNotLinkAncestorToDescendant);
+                ensure!(
+                    !child_is_parent_ancestor,
+                    Error::<T>::CanNotLinkAncestorToDescendant
+                );
+            }
+
+            ChildToParent::<T>::insert((child_collection_id, child_token_id), (parent_collection_id, parent_token_id));
+
+            Ok(())
+        }
     }
 }
 
 impl<T: Config> Module<T> {
     pub fn account_id() -> T::AccountId {
         PALLET_ID.into_account()
+    }
+    fn find_root_owner(
+        child_collection_id: T::Hash,
+        child_token_id: u128,
+    ) -> Result<T::AccountId, DispatchError> {
+        // root token: owner isn't equal with pallet account
+        // if can't find parent token in pallet_nft, it may be burned.
+        let token = <pallet_nft::Module<T>>::tokens(child_collection_id, child_token_id);
+        if token.owner != Self::account_id() {
+            Ok(token.owner)
+        } else {
+            let (parent_collection_id, parent_token_id) =
+                Self::child_to_parent((child_collection_id, child_token_id));
+            ensure!(
+                <pallet_nft::Tokens<T>>::contains_key(parent_collection_id, parent_token_id),
+                Error::<T>::RootTokenNotFound
+            );
+            Self::find_root_owner(parent_collection_id, parent_token_id)
+        }
+    }
+
+    fn is_ancestor(
+        maybe_ancestor_token: (T::Hash, u128),
+        maybe_descendant_token: (T::Hash, u128),
+    ) -> Result<bool, DispatchError> {
+        let (ancestor_collection_id, ancestor_token_id) = maybe_ancestor_token;
+        let (descendant_collection_id, descendant_token_id) = maybe_descendant_token;
+
+        // if can't find descendant token's parent, it walks to the end and the token may be a root token.
+        let have_parent =
+            ChildToParent::<T>::contains_key((descendant_collection_id, descendant_token_id));
+
+        if have_parent {
+            let (parent_collection_id, parent_token_id) =
+                Self::child_to_parent((descendant_collection_id, descendant_token_id));
+
+            // if parent token not in pallet_nft, it may be burned.
+            ensure!(
+                <pallet_nft::Tokens<T>>::contains_key(parent_collection_id, parent_token_id),
+                Error::<T>::RootTokenNotFound
+            );
+
+            // check whether token's parent equal with ancestor token
+            let is_equal = parent_collection_id == ancestor_collection_id
+                && parent_token_id == ancestor_token_id;
+
+            if is_equal {
+                Ok(true)
+            } else {
+                Self::is_ancestor(
+                    (ancestor_collection_id, ancestor_token_id),
+                    (parent_collection_id, parent_token_id),
+                )
+            }
+        } else {
+            Ok(false)
+        }
     }
 }
--- pallets/pallet-graph/src/tests.rs
+++ pallets/pallet-graph/src/tests.rs
@@ -7,7 +7,57 @@ fn create_graph() {
 		// Dispatch a signed extrinsic.
 		let alice_address = 1;
 		let alice = Origin::signed(alice_address);
+        let mint_amount = 10;
+		let parent_token_id = 0;
 
-		assert_ok!(GraphModule::create_graph(alice, vec![2,3,4]>));
+        CollectionModule::create_collection(alice.clone(), vec![2, 3, 3], false).unwrap();
+
+        let nonce = CollectionModule::get_nonce();
+        let child_collection_id = CollectionModule::generate_collection_id(nonce).unwrap();
+
+		CollectionModule::create_collection(alice.clone(), vec![2, 3, 3], false).unwrap();
+
+		let nonce = CollectionModule::get_nonce();
+        let parent_collection_id = CollectionModule::generate_collection_id(nonce).unwrap();
+
+
+        assert_ok!(NFTModule::mint_non_fungible(
+            alice.clone(),
+            alice_address,
+            child_collection_id,
+            vec![2, 3, 3],
+            mint_amount
+        ));
+
+		assert_ok!(NFTModule::mint_non_fungible(
+            alice.clone(),
+            alice_address,
+            parent_collection_id,
+            vec![2, 3, 3],
+            mint_amount
+        ));
+
+		assert_ok!(GraphModule::link(alice.clone(), child_collection_id, 0, parent_collection_id, parent_token_id));
+
+		assert_ok!(GraphModule::link(alice.clone(), child_collection_id, 1, child_collection_id, 0));
+		assert_ok!(GraphModule::link(alice.clone(), child_collection_id, 2, child_collection_id, 1));
+		assert_ok!(GraphModule::link(alice.clone(), child_collection_id, 3, child_collection_id, 2));
+		assert_ok!(GraphModule::link(alice.clone(), child_collection_id, 4, child_collection_id, 3));
+		assert_ok!(GraphModule::link(alice.clone(), child_collection_id, 5, child_collection_id, 4));
+
+
+		let token = NFTModule::tokens(child_collection_id, 2);
+		assert_eq!(token.owner, GraphModule::account_id());
+		let root_owner = GraphModule::find_root_owner(child_collection_id, 1).unwrap();
+		assert_eq!(root_owner, alice_address);
+
+		let is_ancestor = GraphModule::is_ancestor((parent_collection_id, parent_token_id), (child_collection_id, 4)).unwrap();
+		assert_eq!(is_ancestor, true);
+
+		let is_ancestor = GraphModule::is_ancestor((child_collection_id, 2), (child_collection_id, 4)).unwrap();
+		assert_eq!(is_ancestor, true);
+
+		let is_ancestor = GraphModule::is_ancestor((child_collection_id, 5), (parent_collection_id, parent_token_id)).unwrap();
+		assert_eq!(is_ancestor, false);
 	});
 }
